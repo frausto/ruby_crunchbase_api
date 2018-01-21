e46a26ccf8e2db98a87791eb220b8631751f08d0
frausto <nfrausto@fitstar.com>
2018-01-21 12:53:05 -0800

    use model base

--- lib/crunchbase.rb
+++ lib/crunchbase.rb
@@ -1,12 +1,32 @@
 # frozen_string_literal: true
 require 'singleton'
 require 'time'
-
 require 'json'
+require 'crunchbase/version'
 require 'crunchbase/exception'
 require 'crunchbase/api'
 require 'crunchbase/search'
-require 'crunchbase/model'
+require 'crunchbase/model/base'
+
+#models
+require 'crunchbase/model/acquisition'
+require 'crunchbase/model/address'
+require 'crunchbase/model/category'
+require 'crunchbase/model/degree'
+require 'crunchbase/model/fund'
+require 'crunchbase/model/funding_round'
+require 'crunchbase/model/image'
+require 'crunchbase/model/investment'
+require 'crunchbase/model/ipo'
+require 'crunchbase/model/job'
+require 'crunchbase/model/location'
+require 'crunchbase/model/news'
+require 'crunchbase/model/organization'
+require 'crunchbase/model/organization_summary'
+require 'crunchbase/model/person'
+require 'crunchbase/model/person_summary'
+require 'crunchbase/model/video'
+require 'crunchbase/model/website'
 
 module Crunchbase
   API_VERSION     = '3.1'
