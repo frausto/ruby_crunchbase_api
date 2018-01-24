7685ac462c8f560e35ed896dbfd1a79d161e9fac
frausto <nfrausto@fitstar.com>
2018-01-23 20:11:35 -0800

    namespacing issues duh

--- lib/crunchbase/model/base.rb
+++ lib/crunchbase/model/base.rb
@@ -23,7 +23,7 @@ module Crunchbase::Model
         return @relationships[m.to_s] 
       end
       # raise ArgumentError.new("Method `#{m}` doesn't exist.")
-      puts "Method `#{m}` doesn't exist." if API.debug
+      puts "Method `#{m}` doesn't exist." if ::Crunchbase::API.debug
       nil
     end
 
